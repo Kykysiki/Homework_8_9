40f585c65fafbd8043a788fbf6f5f8d423f4c0c6
Глеб <bul.gleb@gmail.com>
2022-09-19 11:20:37 +0300

    Homework_8_9 - Массивы (Урок 1) - Исправлен

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,83 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 1-2");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1-2:");
         int box[] = new int[3]; // 1 массив
-        box[0] = 27;
-        box[1] = 55;
-        box[2] = 12;
+        for (int index = 0; index < box.length; index++) {
+            box[index] = index + 1;
+            if (index == box.length - 1) {
+                System.out.println(box[index]);
+                break;
+            }
+            System.out.print(box[index] + ", ");
+        }
         double[] dimensions = {1.57, 7.654, 9.986}; // 2 массив
-        int recipe[] = new int[2]; // 3 массив
-        int b = 3;
-        recipe[0] = b;
-        recipe[1] = b * 2;
-        int apple = box[0]; // 1 массив - Присваивание значений переменным
-        int orange = box[1];
-        int mango = box[2];
-        System.out.println(apple + ", " + orange + ", " + mango);
-        double height = dimensions[0]; // 2 массив - Присваивание значений переменным
-        double width = dimensions[1];
-        double depth = dimensions[2];
-        System.out.println(height + ", " + width + ", " + depth);
-        int lemon = recipe[0]; // 3 массив - Присваивание значений переменным
-        int sugar = recipe[1];
-        System.out.println(lemon + ", " + sugar);
-        System.out.println("Задача 3");
-        System.out.println(mango + ", " + orange + ", " + apple);
-        System.out.println(depth + ", " + width + ", " + height);
-        System.out.println(sugar + ", " + lemon);
-        System.out.println("Задача 4");
-        if (box[0] % 2 == 0) { // Если остаток от деления 0 - выводим значение числа
-            System.out.println(apple);
-        } else {
-            System.out.println(apple + 1); // Если условие не выполняется, добавляем 1
+        for (int index = 0; index < box.length; index++) {
+            if (index == dimensions.length - 1) {
+                System.out.println(dimensions[index]);
+                break;
+            }
+            System.out.print(dimensions[index] + ", ");
         }
-        if (box[1] % 2 == 0) {
-            System.out.println(orange);
-        } else {
-            System.out.println(orange + 1);
+        int recipe[] = new int[4]; // 3 массив
+        for (int index = 0; index < recipe.length; index++) {
+            recipe[index] = (index + 1) * 2;
+            if (index == recipe.length - 1) {
+                System.out.println(recipe[index]);
+                break;
+            }
+            System.out.print(recipe[index] + ", ");
         }
-        if (box[2] % 2 == 0) {
-            System.out.println(mango);
-        } else {
-            System.out.println(mango + 1);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 3:");
+        int box[] = new int[3]; // 1 массив
+        for (int index = box.length - 1; index >= 0; index--) {
+            box[index] = index + 1;
+            if (index == 0) {
+                System.out.println(box[index]);
+                break;
+            }
+            System.out.print(box[index] + ", ");
+        }
+        double[] dimensions = {1.57, 7.654, 9.986}; // 2 массив
+        for (int index = dimensions.length - 1; index >= 0; index--) {
+            if (index == 0) {
+                System.out.println(dimensions[index]);
+                break;
+            }
+            System.out.print(dimensions[index] + ", ");
+        }
+        int recipe[] = new int[4]; // 3 массив
+        for (int index = recipe.length - 1; index >= 0; index--) {
+            recipe[index] = (index + 1) * 2;
+            if (index == 0) {
+                System.out.println(recipe[index]);
+                break;
+            }
+            System.out.print(recipe[index] + ", ");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 4:");
+        int arr[] = new int[3];
+        for (int index = 0; index < arr.length; index++) {
+            arr[index] = index + 1;
+            if (arr[index] % 2 != 0) { // Если остаток от деления не равен 0 - добавляем 1 к значению
+                arr[index]++;
+            }
+            if (index == arr.length - 1){
+                System.out.println(arr[index]);
+                break;
+            }
+            System.out.print(arr[index]+", ");
         }
     }
 }
